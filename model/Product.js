89691a3f5c46aca7f373f8a49b0d69b2363f6370
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-06-28 17:38:12 +0545

    product model completed

--- model/Product.js
+++ model/Product.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
+const ImageCombinationSchema = new mongoose.Schema({
+  dialColor: { type: String, required: true },
+  bandColor: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+});
+
 const ProductSchema = new mongoose.Schema({
   shortName: { type: String, required: true },
   fullName: { type: String, required: true },
   price: { type: Number, required: true },
+  discountPrice: { type: Number }, // New field for discount
   rating: { type: Number, default: 0 },
-  reviews: [{ type: String }], 
+  reviews: [{ type: String }],
   description: { type: String, required: true },
-  dialColor: { type: String, required: true },
-  bandColor: { type: String, required: true },
   type: { type: String, enum: ["digital", "analog"], required: true },
   inStock: { type: Boolean, default: true },
-  images: [{ type: String, required: true }],
+  imageCombinations: [ImageCombinationSchema], // New logic here
 });
 
 module.exports = mongoose.model("Product", ProductSchema);

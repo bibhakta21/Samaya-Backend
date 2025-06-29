9b6ed0b2c49d96763ad7b736e3a0925ff6ede1b8
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-06-29 22:44:13 +0545

    booking route completed

---
+++ routes/BookingRoute.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+  createBooking,
+  getBookingById,
+  getUserBookings,
+  getAllBookings,
+  cancelBooking,
+  approveBooking,
+  deleteBooking
+} = require("../controller/BookingController");
+
+const { authMiddleware, adminMiddleware } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.post("/", authMiddleware, createBooking);
+router.get("/my-bookings", authMiddleware, getUserBookings);
+router.get("/:id", authMiddleware, getBookingById);
+router.get("/", authMiddleware, adminMiddleware, getAllBookings);
+router.put("/:id/cancel", authMiddleware, cancelBooking);
+router.put("/:id/approve", authMiddleware, adminMiddleware, approveBooking);
+router.delete("/:id", authMiddleware, adminMiddleware, deleteBooking);
+
+module.exports = router;

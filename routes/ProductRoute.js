31ac6db3473b076e94558a9a322d2cad6df07f1c
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-03 15:10:22 +0545

    update route added

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -15,14 +15,16 @@ const upload = require("../middleware/uploadMiddleware");
 
 const router = express.Router();
 
+// Product routes
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
 router.post("/", authMiddleware, adminMiddleware, upload, createProduct);
 router.put("/:id", authMiddleware, adminMiddleware, upload, updateProduct);
 router.delete("/:id", authMiddleware, adminMiddleware, deleteProduct);
 
-// Review routes - protected by authMiddleware only
+
 router.post("/:id/reviews", authMiddleware, addReview);
+router.put("/:id/reviews", authMiddleware, updateReview);
 
 
 module.exports = router;

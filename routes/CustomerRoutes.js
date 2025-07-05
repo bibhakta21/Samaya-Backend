0a53fdd946ff88c2eca2be28548307fb9bbae467
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-05 17:22:30 +0545

    customer route improved

---
+++ routes/CustomerRoutes.js
@@ -0,0 +1,10 @@
+const express=require("express")
+const router=express.Router();
+const {findAll, save,findById,deleteById,update} = require("../controller/CustomerController");
+const CustomerValidation=require("../validation/CustomerValidation")
+
+router.get("/",  findAll);
+router.post("/",CustomerValidation, save);
+
+
+module.exports=router;

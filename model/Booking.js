73fa7e5c1616ba396eab4c891aab8ad2ed360b5e
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-05 17:20:11 +0545

    booking model improvement

--- model/Booking.js
+++ model/Booking.js
@@ -3,16 +3,17 @@ const mongoose = require("mongoose");
 const BookingSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-  productImage: { type: String, required: true },
+  quantity: { type: Number, required: true, default: 1 },
+  productImage: { type: String, default: "" },
   productShortName: { type: String, required: true },
   price: { type: Number, required: true },
-  quantity: { type: Number, required: true },
-  addressOne: { type: String, required: true },
-  country: { type: String, required: true },
-  number: { type: String, required: true },
-  paymentType: { type: String, enum: ["cashondelivery", "khalti"], required: true },
-  status: { type: String, enum: ["pending", "approved", "cancelled"], default: "pending" },
-  bookingDate: { type: Date, default: Date.now }
+  addressOne: { type: String, default: null },
+  country: { type: String, default: null },
+  dialColor: { type: String, default: null },
+  bandColor: { type: String, default: null },
+  status: { type: String, default: "pending" },
+  createdAt: { type: Date, default: Date.now },
 });
+
 
 module.exports = mongoose.model("Booking", BookingSchema);
